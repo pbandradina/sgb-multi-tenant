bd6e0ba140d585b2f7f8f3dd035e02136c4a51ac
pbandradina <pbandradina@gmail.com>
2026-05-27 20:20:29 +0000

    supabase fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-// Carrega variáveis do .env
 dotenv.config();
 
 export default defineConfig({
   schema: "./shared/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
-
   dbCredentials: {
-    // Usa conexão direta para migrações
-    url: process.env.DATABASE_URL_DIRECT!,
+    url: process.env.DATABASE_URL!,
   },
 });
