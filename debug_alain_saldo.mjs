2ad0b1218a5f6deade62da9f0f51d47bd387e622
pbandradina <pbandradina@gmail.com>
2026-05-30 14:42:32 -0300

    Limpeza: removendo arquivos de debug e pastas desnecessárias

--- debug_alain_saldo.mjs
+++
@@ -1,5 +0,0 @@
-// Chama calcularSaldoFMO diretamente para o Alain
-import { calcularSaldoFMO } from './server/db.ts';
-
-const result = await calcularSaldoFMO(30005, 2);
-console.log(JSON.stringify(result, null, 2));
